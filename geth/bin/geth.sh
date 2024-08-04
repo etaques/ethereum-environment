5c2cdfa940f2933907dfe4cdcf9acfeb8dacac5e
Everton H. Taques <97463920+etaques@users.noreply.github.com>
2024-08-04 12:24:20 -0300

    Update geth.sh

--- geth/bin/geth.sh
+++ geth/bin/geth.sh
@@ -10,29 +10,29 @@ network_id=$(jq -r '.config.chainId' /root/holesky/genesis.json)
 exec geth \
   --holesky \
   --port=30303 \
-  --bootnodes "$bootnodes"
-  --http.addr=0.0.0.0
-  --http.port=8545
-  --http
-  --http.api=engine,eth,web3,txpool,net,debug
-  --http.corsdomain="*"
-  --http.vhosts="*"
-  --ws
-  --ws.addr=0.0.0.0
-  --ws.port=8546
-  --ws.api=engine,eth,web3,txpool,net,debug
-  --ws.origins='*'
-  --syncmode=snap
-  --metrics
-  --metrics.addr=0.0.0.0
-  --metrics.port=8300
-  --authrpc.addr=0.0.0.0
-  --authrpc.port=8551
-  --authrpc.vhosts="*"
-  --authrpc.jwtsecret="/root/.jwt/jwtsecret"
-  --verbosity=3
-  --nousb
-  --graphql
-  --graphql.corsdomain='*'
-  --graphql.vhosts='*'
+#  --bootnodes "$bootnodes" \
+  --http.addr=0.0.0.0 \
+  --http.port=8545 \
+  --http \
+  --http.api=engine,eth,web3,txpool,net,debug \
+  --http.corsdomain="*" \
+  --http.vhosts="*" \
+  --ws \
+  --ws.addr=0.0.0.0 \
+  --ws.port=8546 \
+  --ws.api=engine,eth,web3,txpool,net,debug \
+  --ws.origins='*' \
+  --syncmode=snap \
+  --metrics \
+  --metrics.addr=0.0.0.0 \
+  --metrics.port=8300 \
+  --authrpc.addr=0.0.0.0 \
+  --authrpc.port=8551 \
+  --authrpc.vhosts="*" \
+  --authrpc.jwtsecret="/root/.jwt/jwtsecret" \
+  --verbosity=3 \
+  --nousb \
+  --graphql \
+  --graphql.corsdomain='*' \
+  --graphql.vhosts='*' \
   --nodiscover
